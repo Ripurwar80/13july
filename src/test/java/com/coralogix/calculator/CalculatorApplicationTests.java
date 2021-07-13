cb50372a788d2c852603a2595831906e13325781
Ripurwar80 <34250719+Ripurwar80@users.noreply.github.com>
2021-07-13 21:47:00 +0530

    Update CalculatorApplicationTests.java

--- src/test/java/com/coralogix/calculator/CalculatorApplicationTests.java
+++ src/test/java/com/coralogix/calculator/CalculatorApplicationTests.java
@@ -1,9 +1,9 @@
 package com.coralogix.calculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CalculatorApplicationTests {
 
 	@Test
