60087447245bc3328dfc06fc5a1477b03de4bf42
Ripurwar80 <34250719+Ripurwar80@users.noreply.github.com>
2021-07-13 17:31:31 +0530

    Update AddServiceTest.java

--- src/test/java/com/coralogix/calculator/services/AddServiceTest.java
+++ src/test/java/com/coralogix/calculator/services/AddServiceTest.java
@@ -1,20 +1,17 @@
 package com.coralogix.calculator.services;
 
-import com.coralogix.calculator.model.Result;
+iimport org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class AddServiceTest {
-
+public class testing {
     @Test
-    public void addIntegers() {
-        // given
-        AddService service = new AddService();
-
-        // when
-        Result result = service.add(2, 3);
-
-        // then
-        assertEquals(result.getResult(), 5);
+    public void evaluatesExpression() {
+        sum calculator = new sum();
+        int add = calculator.add(5,4);
+        System.out.println("Test 1");
+        Assertions.assertEquals(9, add, "The sum is correct");
+        add = calculator.add(6,9);
+        Assertions.assertEquals(15, add, "The sum is correct");
+        System.out.println("Test 2");
     }
 }
