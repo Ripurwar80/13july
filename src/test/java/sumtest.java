8ee1264fd38fab37f5c49654393fed4538c30014
Ripurwar80 <34250719+Ripurwar80@users.noreply.github.com>
2021-07-13 21:55:03 +0530

    Update and rename src/test/java/com/coralogix/calculator/CalculatorApplicationTests.java to src/test/java/sumtest.java

---
+++ src/test/java/sumtest.java
@@ -0,0 +1,18 @@
+package com.coralogix.calculator;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+class sumtest {
+
+	@Test
+	sum calculator = new sum();
+        int total = calculator.add(5, 4);
+        System.out.println("Test_1");
+        Assertions.assertEquals(9, total, "The sum is correct");
+        total = calculator.add(6, 9);
+        Assertions.assertEquals(15, total, "The sum is correct");
+        System.out.println("Test_2");
+
+}
